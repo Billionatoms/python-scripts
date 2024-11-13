021eb4cf9d2add4a85cdfb5014c1a9cdc7c8c641
billionatoms <ss007ds@gmail.com>
2024-11-13 16:49:58 +0000

    changed the path to the root file for local use.

--- root2hdf5/checking_root.py
+++ root2hdf5/checking_root.py
@@ -4,7 +4,7 @@ import uproot
 
 # %%
 # Open the Root file
-f = uproot.open("/eos/user/s/ssaini/muonc/btagging/samples/v0.0.1/mumu_H_bb_10TeV.00000.lctuple.root")
+f = uproot.open("/mnt/c/Users/Saurabh/cernbox/muonc/btagging/samples/v0.0.1/mumu_H_bb_10TeV.00000.lctuple.root")
 
 
 # %%
